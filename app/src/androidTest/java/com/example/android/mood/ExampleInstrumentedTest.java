b9b57264543c91bd83c8fea8e3af03534f841b8d
Jose Garcia <jogarcia809@yahoo.com>
2018-04-11 07:13:18 -0400

    removed tests

--- app/src/androidTest/java/com/example/android/mood/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/android/mood/ExampleInstrumentedTest.java
@@ -6,19 +6,12 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.example.android.mood.room.MoodDatabase;
-import com.example.android.mood.watson.WatsonHelper;
 import com.example.android.mood.watson.WatsonListener;
-import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.Tone;
-import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneOptions;
 import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneScore;
 
-import org.apache.commons.io.IOUtils;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.io.IOException;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -46,17 +39,6 @@ public class ExampleInstrumentedTest implements WatsonListener {
         assertNotEquals(0, database.weatherPoetryDao().getAll().size());
     }
 
-    @Test
-    public void testWatson() {
-        WatsonHelper helper = WatsonHelper.getInstance();
-
-        helper.configureToneAnalyzer(appContext);
-
-        ToneOptions options = new ToneOptions.Builder().addTone(Tone.EMOTION).html(false).build();
-
-        helper.getTones(getTestPoem(), options, this);
-    }
-
     public String getTestPoem() {
         return MoodDatabase.getInstance(appContext).weatherPoetryDao().getAll().get(0).getPoem();
     }
@@ -66,18 +48,4 @@ public class ExampleInstrumentedTest implements WatsonListener {
         assertNotEquals(0, tonesScoreList.size());
     }
 
-    @Test
-    public void checkCredentials() {
-        try {
-
-            JSONObject credentialsJSON = new JSONObject(IOUtils.toString(appContext.getResources().openRawResource(R.raw.credentials), "UTF-8"));
-            assertEquals("OtPtrc0Pdpl2", credentialsJSON.getString("password"));
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
 }
